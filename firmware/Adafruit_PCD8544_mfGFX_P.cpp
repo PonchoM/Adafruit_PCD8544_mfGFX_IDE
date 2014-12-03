b0c88afa19aad73083c98cfed7e61d7d8181ff0e
PonchoM <poncho@bitshake.com.mx>
2014-12-03 15:42:18 -0600

    Update Adafruit_PCD8544_mfGFX_P.cpp

--- firmware/Adafruit_PCD8544_mfGFX_P.cpp
+++ firmware/Adafruit_PCD8544_mfGFX_P.cpp
@@ -19,8 +19,8 @@ Adapted for Spark Core by Paul Kourany, April 2014
 *********************************************************************/
 
 
-#include "../Adafruit_mfGFX/Adafruit_mfGFX.h"
-#include "Adafruit_PCD8544_mfGFX.h"
+#include "../Adafruit_mfGFX/Adafruit_mfGFX_P.h"
+#include "Adafruit_PCD8544_mfGFX_P.h"
 
 // the memory buffer for the LCD
 uint8_t pcd8544_buffer[LCDWIDTH * LCDHEIGHT / 8] = {
